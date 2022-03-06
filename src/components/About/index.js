8594c432d73ebb5e5a51d80a6cc7f0e304cfd462
Mark Dale <msdaledad@gmail.com>
2022-03-06 00:34:30 -0500

    completed about...working on nav

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import coverImage from "../../assets/cover/cover-image.jpg";
+import pdf from "../../assets/documents/markdale_2021.pdf";
+
+function About() {
+  return (
+    <section className="my-5">
+      <p>Let us run with endurance the race set before us...</p>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <h1 id="about">About me</h1>
+      <div className="my-2">
+        <p>
+          I'm an experienced software engineer who has been in the field for 38 years.  I've focused most of my career
+          on backend development managing and handling large data stores.  The past 22 years at Walt Disney World Parks
+          and Resorts IT, I designed, implemented, and supported database applications focused on managing guest
+          reservations and other linked guest data. The number of guests supported in the system grew from ~80 million
+          to over 200 million guests during my tenure. A concise breakdown of that experience is provided in my resume
+          (<a href={pdf}>View Pdf</a>). My latest technical pursuit has been acquiring a Full-Stack skill set so I
+          can be more engaged in development effecting both sides of the browser. I'm hard-working, focused, and work
+          well alone, yet I enjoy being part of, and contributing to a well functioning team. Perhaps I can be a part
+          of yours. Thanks for visiting my site, and I hope to hear from you.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default About;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import profileImage from "../../assets/profile/profile-pic.png";
+
+
+function Nav() {
+
+  return (
+    <header>
+      <nav className="flex-row">
+        <h1>
+          <img id="my-profile" src={profileImage} alt="profile" />
+            Mark Dale
+        </h1>
+        <ul className="flex-row center-content">
+          <li className="mx-2">
+            <a href="#about">
+              About me
+            </a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          <li>
+            <span>Projects</span>
+          </li>
+          <li>
+            <span>Resume</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
